e118ff3cacff0e14c0cdfbca35f70be0bc0d3aee
Vasiliy.Nevedomskiy <nevedomsky.vasily@yandex.ru>
2023-07-17 14:07:24 +0400

    изменил celebrate для user и  movie

--- routes/movies.js
+++ routes/movies.js
@@ -14,8 +14,18 @@ router.post(
   '/',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required().custom(validIsURL),
+      country: Joi.string().required(),
+      director: Joi.string().required(),
+      duration: Joi.number().required(),
+      year: Joi.number().required(),
+      description: Joi.string().required(),
+      image: Joi.string().required().custom(validIsURL),
+      trailerLink: Joi.string().required().custom(validIsURL),
+      thumbnail: Joi.string().required().custom(validIsURL),
+      owner: Joi.required(),
+      movieId: Joi.number().required(),
+      nameRU: Joi.string().required(),
+      nameEN: Joi.string().required(),
     }),
   }),
   createMovie,
@@ -25,7 +35,7 @@ router.delete(
   '/:id',
   celebrate({
     params: Joi.object().keys({
-      id: Joi.string().hex().length(24),
+      id: Joi.string().hex().length(24).required(),
     }),
   }),
   removeMovieById,
