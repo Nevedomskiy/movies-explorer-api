7f4579c0a843a755ca1573bc52a4452071d85f81
Vasiliy.Nevedomskiy <nevedomsky.vasily@yandex.ru>
2023-08-04 10:19:00 +0400

    отключил проверку шифрования для разработки

--- controllers/users.js
+++ controllers/users.js
@@ -74,7 +74,7 @@ const login = (req, res, next) => {
         {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          secure: true,
+          // secure: true, отключил для разработки
         },
       );
       res.status(200).send({ message: messageSuccessfulLogin });
