404c82f09beddafa67787ea964a7bd016f49754e
Vasiliy.Nevedomskiy <nevedomsky.vasily@yandex.ru>
2023-10-03 10:41:03 +0400

    добавил фронтенд

--- frontend/src/blocks/ButtonHome/ButtonHome.jsx
+++
@@ -1,8 +0,0 @@
-import { Link } from 'react-router-dom';
-import './ButtonHome.css';
-
-function ButtonHome({ modification }) {
-  return <Link to="/" className={`button-home ${modification}`}></Link>;
-}
-
-export default ButtonHome;
--- frontend/src/blocks/Title/Title.jsx
+++
@@ -1,7 +0,0 @@
-import './Title.css';
-
-function Title({ text, modification }) {
-  return <h2 className={`title ${modification}`}>{text}</h2>;
-}
-
-export default Title;
