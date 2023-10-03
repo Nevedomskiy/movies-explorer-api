23412afa95f22694144e6cbfe9ff92f83acaf540
Vasiliy.Nevedomskiy <nevedomsky.vasily@yandex.ru>
2023-10-03 10:30:21 +0400

    добавил фронтенд

---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,52 @@
+import './Login.css';
+import { INPUT_LIST_lOGIN } from '../../utils/constants/constants';
+import useInput from '../../utils/Validation/Validation';
+import FormSign from '../../blocks/FormSign/FormSign';
+
+function Login({
+  logIn,
+  location,
+  textServerError,
+  setTextServerError,
+  isLoading,
+}) {
+  const email = useInput('', {
+    isEmpty: true,
+    isEmail: true,
+  });
+
+  const password = useInput('', {
+    isEmpty: true,
+    minLength: 8,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email.value || !password.value) {
+      return;
+    }
+    logIn(email.value, password.value);
+  };
+
+  return (
+    <div className="login">
+      <FormSign
+        inputList={INPUT_LIST_lOGIN}
+        title={'Рады видеть!'}
+        nameButtonSubmit={'Войти'}
+        textAfterButton={'Ещё не зарегистрированы? '}
+        textLink={'Регистрация'}
+        linkValue={'sign-up'}
+        email={email}
+        location={location}
+        password={password}
+        textServerError={textServerError}
+        setTextServerError={setTextServerError}
+        handleSubmit={handleSubmit}
+        isLoading={isLoading}
+      ></FormSign>
+    </div>
+  );
+}
+
+export default Login;
---
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,23 @@
+import './NotFound.css';
+import { useNavigate } from 'react-router-dom';
+
+function NotFound() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="not-found">
+      <div className="not-found__container">
+        <h1 className="not-found__title">404</h1>
+        <p className="not-found__description">Страница не найдена</p>
+      </div>
+      <button
+        onClick={() => navigate(-1)}
+        className="button not-found__comeback hover-link"
+      >
+        <p className="not-found__text-btn">Назад</p>
+      </button>
+    </div>
+  );
+}
+
+export default NotFound;
